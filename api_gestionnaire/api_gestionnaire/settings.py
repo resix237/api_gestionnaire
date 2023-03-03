b2df06a47fdf5e7f4a396cd6d730b6d9ebfc51ab
resix237 <resix237pro@gmail.com>
2023-03-03 10:11:27 +0100

    administration terminé

--- api_gestionnaire/api_gestionnaire/settings.py
+++ api_gestionnaire/api_gestionnaire/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'api_gestionnaire.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,7 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
